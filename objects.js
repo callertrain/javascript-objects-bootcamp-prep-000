4bb55c87413d8388de0db23c4350373260207c93
Carla Marques <cahcardeal@gmail.com>
2017-09-28 21:29:39 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,3 +2,8 @@ var playlist = {
     aha: 'Take On Me',
     panic: 'Death of A Bachelor'
 };
+
+function updatePlaylist(playlist, artistName, songTitle){
+
+  var playlist = {artistName + " " + songTitle};
+}
